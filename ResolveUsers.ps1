9d41de0f02f6f872929ddbc71cf34cf0b9368287
idanoh33 <idanoh33@gmail.com>
2017-12-03 14:54:26 +0200

    Add files via upload

---
+++ ResolveUsers.ps1
@@ -0,0 +1,46 @@
+﻿<#
+.Synopsis
+   The script will reslove users from the groups are listed on groupList file
+.DESCRIPTION
+   The script will reslove users from the groups are listed on groupListFile (by default c:\temp\groupList.txt)
+   and will export it to c:\temp\GroupList\$group.csv
+.EXAMPLE
+   Example of how to use this cmdlet
+.EXAMPLE
+   Another example of how to use this cmdlet
+#>
+    [CmdletBinding()]
+
+    Param
+    (
+        # Param1 help description
+        [Parameter(ValueFromPipelineByPropertyName=$true,
+                   Position=0)]
+        $groupListFile= "c:\temp\groupList.txt"
+
+        # Param2 help description
+        
+        
+    )
+
+    Begin
+    {
+    $Groups = cat $groupListFile
+    }
+    Process
+    {
+    foreach ($Group in $Groups)
+        {
+            $GroupMembers = Get-ADGroupMember -Identity $Group -Recursive
+            foreach ($Member in $GroupMembers)
+                {
+                    $path = Split-Path $groupListFile
+                    Get-ADUser -Identity $Member -Properties office | `
+                    Select SamAccountName,Office | `
+                    Export-Csv ("$path\$Group.csv").Replace(" ","") -Append -Force 
+                }
+        }
+    }
+    End
+    {
+    }
