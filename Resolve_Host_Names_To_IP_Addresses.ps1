7d80791ac45c2f9757a743ef6936707e011e5290
i737208 <idan.ohayon@allscripts.com>
2017-12-27 10:03:09 +0200

    add Resolve_Host_Names_To_IP_Addresses.ps1

---
+++ Resolve_Host_Names_To_IP_Addresses.ps1
@@ -0,0 +1,22 @@
+﻿$SourceFile= "C:\temp\Host's_list.txt"
+$OutputFile= "C:\temp\IP's_list.txt"
+
+function Get-HostToIP($hostname) {    
+    # Ignore error
+    $ErrorActionPreference = 'ignore'
+    
+    # Resolve hostname to IP
+    $result = [system.Net.Dns]::GetHostByName($hostname)
+
+    # if found retun name + IP address
+    if ($result -ne $null){"$hostname " + $result.AddressList.IPAddressToString}
+
+    # Else return "Hostname was not found"
+    else {Write-Output "$hostname was not found"}
+}
+
+# Run the function for every line in the txt file and create an outputfile
+Get-Content $SourceFile | ForEach-Object {(Get-HostToIP($_))>> $OutputFile}
+
+# Open foutput file on notepad
+notepad $OutputFile
