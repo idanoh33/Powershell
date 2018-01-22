60cfbaa7ca15e840be4cb7c2ffb4bba768399a74
Idan Ohayon <idanoh33@gmail.com>
2018-01-22 12:26:58 +0200

    Adding Import-certificateOnVM

---
+++ Import-CertificateOnVM.ps1
@@ -0,0 +1,27 @@
+﻿    Param
+    (
+        # Param1 help description
+        [Parameter(Mandatory=$true,
+                   ValueFromPipelineByPropertyName=$true,
+                   Position=0)]
+        $certificatePath,
+
+        # Param2 help description
+        
+        $destinationServer
+    )
+    
+# resolve certificate name from path
+$certificateName= split-path $certificatePath -Leaf
+
+# copy certificate to remote server
+Copy-VMGuestFile -Source $certificatePath -Destination $env:ProgramData -VM VM -GuestToLocal -GuestUser administrator -GuestPassword pass2
+
+
+# Set script content
+$script = @"
+Import-Certificate -FilePath "$env:ProgramData\$certificateName" -CertStoreLocation Cert:\LocalMachine\my
+"@
+
+# Import certificate on remote guest
+Invoke-VMScript -VM VM -ScriptText $script -GuestUser administrator -GuestPassword pass2
